d1cda98c873606f25a1ed64c2ee4a400d061fbeb
Fabiano Furlan <fabiano.furlanf@gmail.com>
2020-06-09 16:09:17 -0300

    feat(enum): add enum fields

---
+++ Architecture/Enum/person-entity.js
@@ -0,0 +1,18 @@
+const RELATION_TYPES = {
+  MOTHER: "Mãe",
+  FATHER: "Pai",
+  SPOUSE: "Cônjuge",
+  CHILD: "Filho ou Filha",
+  SIBLING: "Irmão ou Irmã",
+  PARTNER: "Sócio",
+  REPRESENTATIVE: "Representante",
+};
+
+const PERSON_TYPES = {
+  PF: "PESSOA FISICA",
+  PJ: "PESSOA JURIDICA",
+};
+
+const PERSON_FILES = {
+  DOCUMENTS: {},
+};
---
+++ Architecture/Enum/property.js
@@ -0,0 +1,32 @@
+const PROPERTY_TYPES = {
+  APARTMENT: "Apartamento",
+  HOME: "Casa",
+  COMMERCIAL: "Comercial",
+};
+
+const RESIDENTS = {
+  THIRD_PARTIES: "Terceiros",
+  OWN: "Próprio",
+};
+
+const FILE_TYPES = {
+  PHOTOS: {},
+  DOCUMENTS: {},
+};
+
+const PROPERTY_AGE = [
+  "<=2",
+  "3-5",
+  "6-10",
+  "11-20",
+  "21-30",
+  "31-40",
+  "41-50",
+  ">=51",
+];
+
+const BEDROOMS = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+const SUITES = ["0", ...BEDROOMS];
+
+const GARAGES = ["1", "2", "3", "4", "5+"];
